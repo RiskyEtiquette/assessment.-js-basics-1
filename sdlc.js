b9c95fea3bede000dc3684e1b95b2cbe64e7ac2f
Kaden Davis <kadentreyd@gmail.com>
2024-01-22 23:17:07 -0700

    completed sdlc.js

--- sdlc.js
+++ sdlc.js
@@ -5,17 +5,27 @@
     Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
 */
 
-// Implementation
-// Design
-// Maintain
-// Analysis of Requirements
-// Testing & Integration
 // Planning
-
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintenance
 
 //////////////////PROBLEM 2////////////////////
 /*
     Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
     The first one is done for you. 
 */
-const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const planning =
+  "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added.";
+const analysisOfRequirements =
+  "This is the second step, in which the developers gather and analyze the functional and non-functional requirements of the software. They identify the scope, feasibility, risks, and constraints of the project. They also communicate with the stakeholders and users to understand their needs and expectations.";
+const design =
+  "This is the third step, in which the developers design the architecture and structure of the software. They define the modules, components, interfaces, and data models of the software. They also select the technologies, tools, and frameworks to use for the development.";
+const implementation =
+  "This is the fourth step, in which the developers write the code and implement the functionality of the software. They follow the design specifications and coding standards. They also perform unit testing and debugging to ensure the quality of the code.";
+const testingAndIntegration =
+  "This is the fifth step, in which the developers test and integrate the software components. They perform various types of testing, such as integration, system, performance, and user acceptance testing, to verify the functionality, reliability, and usability of the software. They also fix any defects or errors found during the testing.";
+const maintenance =
+  "This is the sixth and final step, in which the developers deploy and maintain the software. They monitor and evaluate the performance and feedback of the software. They also provide technical support and updates to the software. They may also implement new features or enhancements to the software based on the changing requirements or user feedback.";
